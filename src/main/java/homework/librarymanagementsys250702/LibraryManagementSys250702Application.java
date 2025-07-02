416942c54ec024bd40603e53b6a9477d84c7f43f
dongshan24720 <3373558566@qq.com>
2025-07-02 22:17:09 +0800

    Initialize

---
+++ src/main/java/homework/librarymanagementsys250702/LibraryManagementSys250702Application.java
@@ -0,0 +1,13 @@
+package homework.librarymanagementsys250702;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementSys250702Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagementSys250702Application.class, args);
+    }
+
+}
---
+++ src/test/java/homework/librarymanagementsys250702/LibraryManagementSys250702ApplicationTests.java
@@ -0,0 +1,13 @@
+package homework.librarymanagementsys250702;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSys250702ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
